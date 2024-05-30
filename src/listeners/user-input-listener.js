4f47e06cca1a5d16149e38b4dcafb4b516883e7c
jgchoti <jgchotirat@gmail.com>
2024-05-30 12:29:25 +0200

    Update user-input-listener.js

--- src/listeners/user-input-listener.js
+++ src/listeners/user-input-listener.js
@@ -2,8 +2,6 @@ import { userInputHandler } from '../handlers/user-input-handler.js';
 
 export const btnListener = (id = '') => {
     document.getElementById(id).addEventListener('click', userInputHandler);
-
-
 };
 
 export const inputListener = (id = '') => {
