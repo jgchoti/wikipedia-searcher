b4b8310d9c6b09559308c5b85d4d9b325b5818bd
jgchoti <jgchotirat@gmail.com>
2024-05-30 12:29:18 +0200

    Update render-suggestion.js

--- src/components/render-suggestion.js
+++ src/components/render-suggestion.js
@@ -1,8 +1,6 @@
-import { WIKI_URL } from '../../config.js'
+
 import { renderError } from './render-error.js';
-import { renderResultButton } from './render-result-button.js';
-import { renderResultHeader } from './render-result-header.js';
-import { renderResultText } from './render-result-text.js';
+
 /**
  * Document me!
  *
@@ -11,9 +9,8 @@ import { renderResultText } from './render-result-text.js';
 export const renderSuggestion = (results = []) => {
     console.log(results)
 
-    const outputContainer = document.getElementById('output')
-    const container = document.createElement('div')
-
+    const outputContainer = document.getElementById('suggest-search')
+    outputContainer.innerHTML = ""
     if (results.length === 0) {
         renderError('No result found :(')
         return
@@ -32,9 +29,9 @@ export const renderSuggestion = (results = []) => {
         buttonEl.innerText = suggestTitles[i]
         buttonEl.className = "suggest-search"
         aEl.appendChild(buttonEl)
-        container.appendChild(aEl)
+        outputContainer.appendChild(aEl)
 
-        outputContainer.appendChild(container)
+
     }
 
 }
