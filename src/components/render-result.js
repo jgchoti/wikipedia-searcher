867efe90c0765450c65fd0ed23034489edbdb557
jgchoti <jgchotirat@gmail.com>
2024-05-30 12:29:16 +0200

    Update render-result.js

--- src/components/render-result.js
+++ src/components/render-result.js
@@ -2,21 +2,35 @@ import { WIKI_URL } from '../../config.js'
 import { renderError } from './render-error.js';
 import { renderResultButton } from './render-result-button.js';
 import { renderResultHeader } from './render-result-header.js';
+import { renderResultsPerPage } from './render-result-per-page.js';
 import { renderResultText } from './render-result-text.js';
 /**
  * Document me!
  *
  * @returns
  */
-export const renderResult = (results = []) => {
+export const renderResult = (results = [], currentPage, resultsPerPage) => {
     const outputContainer = document.getElementById('output')
+
+    let resultContainer = document.getElementById('result-container');
+    if (!resultContainer) {
+        resultContainer = document.createElement('div');
+        resultContainer.id = 'result-container';
+        outputContainer.appendChild(resultContainer);
+    } else {
+        resultContainer.innerHTML = '';
+    }
+
 
     if (results.length === 0) {
         renderError('No result found :(')
         return
     }
 
-    results.forEach(result => {
+    let currentResult = renderResultsPerPage(results, currentPage, resultsPerPage)
+
+
+    currentResult.forEach(result => {
         const container = document.createElement('div')
         container.classList.add('row')
         const wikiLink = `${WIKI_URL}${result.title}`
@@ -33,6 +47,10 @@ export const renderResult = (results = []) => {
         const aEl = renderResultButton(result, wikiLink)
         container.appendChild(aEl)
 
-        outputContainer.appendChild(container)
+        resultContainer.appendChild(container)
     })
 }
+
+
+
+
